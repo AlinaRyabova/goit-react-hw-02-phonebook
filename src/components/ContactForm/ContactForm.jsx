4523d9a5af4c4eb884436204a7e320a275f39097
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-05-25 10:47:53 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -42,6 +42,9 @@ class ContactForm extends Component {
             value={this.state.name}
             onChange={this.handleChange}
             id={this.contactNameId}
+            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            // required
           />
         </label>
         <label className={style.label} htmlFor={this.contactNumberId}>
@@ -53,6 +56,9 @@ class ContactForm extends Component {
             value={this.state.number}
             onChange={this.handleChange}
             id={this.contactNumberId}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
           />
         </label>
 
